2f397eada0acc0ab1b75dc67367b49eb45647533
chris <chris@192.168.5.150>
2020-03-04 14:17:06 +0000

    REST exception tests

--- bpdts-test/src/main/java/com/bpdts/bpdts_test/rest/RestDAO.java
+++ bpdts-test/src/main/java/com/bpdts/bpdts_test/rest/RestDAO.java
@@ -2,6 +2,8 @@ package com.bpdts.bpdts_test.rest;
 
 import java.util.List;
 
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.cxf.jaxrs.client.WebClient;
@@ -15,18 +17,18 @@ public class RestDAO implements DAO {
 	private static final String baseUrl = "https://bpdts-test-app.herokuapp.com";
 	private Gson gson = new Gson();
 
-
 	/**
 	 * Returns list of all users
 	 *
 	 */
-	public List<Person> getUsers() {
-		
+	public List<Person> getUsers() throws WebApplicationException {
+
 		WebClient client = WebClient.create(baseUrl).path("users").accept(MediaType.APPLICATION_JSON);
-		
+
 		String jsonResult = client.get(String.class);
-		
-		List<Person> personList = gson.fromJson(jsonResult, new TypeToken<List<Person>>(){}.getType());
+
+		List<Person> personList = gson.fromJson(jsonResult, new TypeToken<List<Person>>() {
+		}.getType());
 		
 		return personList;
 	}
@@ -35,14 +37,14 @@ public class RestDAO implements DAO {
 	 * Returns person object for given input id
 	 *
 	 */
-	public Person getUserDetails(int id) {
-		
+	public Person getUserDetails(int id) throws WebApplicationException, NotFoundException {
+
 		WebClient client = WebClient.create(baseUrl).path("user/{id}", id).accept(MediaType.APPLICATION_JSON);
-		
+
 		String jsonResult = client.get(String.class);
-		
+
 		Person person = gson.fromJson(jsonResult, Person.class);
-		
+
 		return person;
 	}
 
@@ -50,14 +52,15 @@ public class RestDAO implements DAO {
 	 * Returns list of all persons for given city
 	 * 
 	 */
-	public List<Person> getUsersByCity(String city) {
-		
+	public List<Person> getUsersByCity(String city) throws WebApplicationException {
+
 		WebClient client = WebClient.create(baseUrl).path("city/{city}/users", city).accept(MediaType.APPLICATION_JSON);
-		
+
 		String jsonResult = client.get(String.class);
-		
-		List<Person> personList = gson.fromJson(jsonResult, new TypeToken<List<Person>>(){}.getType());
-		
+
+		List<Person> personList = gson.fromJson(jsonResult, new TypeToken<List<Person>>() {
+		}.getType());
+
 		return personList;
 	}
 
--- bpdts-test/src/test/java/com/bpdts/bpdts_test/AppTest.java
+++ bpdts-test/src/test/java/com/bpdts/bpdts_test/AppTest.java
@@ -2,14 +2,18 @@ package com.bpdts.bpdts_test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
+
+import javax.ws.rs.WebApplicationException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.bpdts.bpdts_test.entities.Person;
+import com.bpdts.bpdts_test.rest.RestDAO;
 
 public class AppTest {
 
@@ -18,20 +22,39 @@ public class AppTest {
 	}
 
 	@Test
-	public void testBpdtsProcessor() {
-		App app = new App();
+	public void testGetUsersWithin50milesLondon() {
+		App app = new App(new RestDAO());
 		
-		List<Person> personWithin50MileList =  app.bpdtsProcessor();
+		List<Person> personWithin50MileList =  app.getUsersWithin50milesLondon();
 		
 		assertTrue(personWithin50MileList.size() > 0);
 				
 	}
+	
+	@Test
+	public void testRestError() {
+				
+		RestDAO mockedRestDAO = mock(RestDAO.class);
+		
+		when(mockedRestDAO.getUsers()).thenThrow(new WebApplicationException("TEST"));
+		
+		App app = new App(mockedRestDAO);
+		
+		String result = app.bpdtsProcessor();
+		
+		System.out.println("Result is " + result);
+		
+		assertTrue(result.contains("TEST"));
+		
+		
+	}
+	
 
 	@Test
 	public void testIsWithin50MilesOfLondon() {
 		
 		
-		App app = new App();
+		App app = new App(new RestDAO());
 		// Test within 50 miles of London
 		
 		Person person = new Person();
